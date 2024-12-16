51b7ab96f785537f08671d3eddc022d49af12cfc
Leusin <leusin@naver.com>
2024-12-16 18:58:53 +0900

    Add  Board Class

---
+++ Console2048DotCS/Board.cs
@@ -0,0 +1,46 @@
+﻿namespace Console2048DotCS
+{
+    public class Board
+    {
+        const int BOARD_SIZE = 4;
+        int[,] gameBoard = new int[BOARD_SIZE, BOARD_SIZE];
+
+        private Random _rand = new Random();
+
+        public void InitGame()
+        {
+            for (int i = 0; i < BOARD_SIZE; i++)
+            {
+                for (int j = 0; j < BOARD_SIZE; j++)
+                {
+                    gameBoard[i, j] = 0;
+                }
+            }
+
+            int count = 0;
+            while (count < 2)
+            {
+                int x = _rand.Next() % BOARD_SIZE;
+                int y = _rand.Next() % BOARD_SIZE;
+
+                if (gameBoard[x, y] == 0)
+                {
+                    gameBoard[x, y] = (_rand.Next() % 2 + 1) * 2;
+                    count++;
+                }
+            }
+        }
+
+        public void Rander()
+        {
+            for (int i = 0; i < BOARD_SIZE; i++)
+            {
+                for (int j = 0; j < BOARD_SIZE; j++)
+                {
+                    Console.Write($"{gameBoard[i, j]} ");
+                }
+                Console.Write($"\n");
+            }
+        }
+    }
+}
--- Console2048DotCS/Program.cs
+++ Console2048DotCS/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Console2048DotCS;
+using System.Text;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("ESC 키를 누르면 프로그램이 종료됩니다.");
+
+
+int playerScore = 0;
+int moveSocre = 0;
+
+var board = new Board();
+
+board.InitGame();
+
+Console.CursorVisible = false; // 커서를 숨깁니다
+var key = Console.ReadKey(true); // true: 입력한 키를 화면에 표시하지 않음
+
+while (true)
+{
+    // 키 입력이 있을 때만 처리
+    if (Console.KeyAvailable != true)
+    {
+        continue;
+    }
+
+        if (key.Key == ConsoleKey.Escape)
+    {
+        break;
+    }
+
+    {   
+        Console.SetCursorPosition(0, 0);
+        Console.WriteLine($"입력한 키: {key.Key}");
+
+        board.Rander();
+    }
+}
+// 프로그램 종료 후 커서 표시
+Console.CursorVisible = true;
